03cf1d01b7320f5fd8d5c84da1d4e597f2984c71
WilsDevorg <wilsdevorg@gmail.com>
2026-06-04 21:44:40 +0000

    Remove transaction module and test script

--- src/routes/transaction.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authMiddleware = require("../middleware/authMiddleware");
-const transactionController = require("../controllers/transaction.controller");
-
-router.get("/", authMiddleware, transactionController.getTransactions);
-
-module.exports = router;
